a173f063f761f57ba026b4edd4821f3b1b389f71
KaruzG <josemiguelcpcs@gmail.com>
2026-02-04 19:29:32 +0100

    feat: Added skeleton for loading

---
+++ components/Loading.tsx
@@ -0,0 +1,10 @@
+const Loading = () => {
+    return (
+        <>
+            <div className="animate-pulse bg-background-100 m-2 h-48 w-full rounded-lg"></div>
+            <div className="animate-pulse bg-background-100 m-2 h-5 w-[60%] rounded-lg"></div>
+        </>
+    )
+}
+
+export default Loading;
